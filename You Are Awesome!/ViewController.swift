88687d93f0fbe727c43395bef29e80df0d5fbfda
Hailey Yamasaki <hyamasaki@zagmail.gonzaga.edu>
2020-09-02 15:16:42 -0700

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Hailey Yamasaki on 9/2/20.
+//  Copyright © 2020 Hailey Yamasaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
